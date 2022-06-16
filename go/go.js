f6e1d7243ed4b415b533b3a08ccdbae66a9e37c0
Assaideas0419 <laiweisai.60105@gmail.com>
2022-06-16 15:30:29 +0800

    Update go.js

--- go/go.js
+++ go/go.js
@@ -1,17 +1,17 @@
 function setup() {
     initializeFields();
-    createCanvas(450, 450);
+    createCanvas(1000, 1000);
 }
 var bx;
 var by;
 var N;
 function draw() {
     background(color(0xFC, 0xD4, 0x96));
-    for (var y = 50; y < 450; y += 50) {
-        line(0, y, 450, y);
+    for (var y = 50; y < 1000; y += 50) {
+        line(0, y, 1000, y);
     }
-    for (var x = 50; x < 450; x += 50) {
-        line(x, 0, x, 450);
+    for (var x = 50; x < 1000; x += 50) {
+        line(x, 0, x, 1000);
     }
     for (var i = 0; i < N; i++) {
         if (i % 2 == 0)
